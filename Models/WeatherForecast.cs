0253a9d85d68b663aafe4b8cb2bc14aa96a7fd3b
Pawan Jindal <45796558+darena-pjindal@users.noreply.github.com>
2023-09-12 17:34:03 -0500

    Initial commit

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp8
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    public record CustomerInvite()
+    {
+        [Required] public string FirstName { get; set; } = "test";
+        [Required] public string Email { get; set; } = "test";
+
+        public Mail? Mail1 { get; set; } 
+        public Mail? Mail2 { get; set; }
+    }
+
+    public record Mail
+    {
+
+        [Required]
+        public string Address { get; set; }
+       
+    }
+}
